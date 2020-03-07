bcc59e37a4ea7ec5196f07ffeef2287575e1d7cd
Michael Ritter <shake@umiacs.umd.edu>
2020-03-06 17:28:17 -0700

    Use constants where possible

--- src/test/java/org/duraspace/bagit/BagProfileTest.java
+++ src/test/java/org/duraspace/bagit/BagProfileTest.java
@@ -4,6 +4,27 @@
  */
 package org.duraspace.bagit;
 
+import static org.duraspace.bagit.BagConfig.ACCESS_KEY;
+import static org.duraspace.bagit.BagConfig.BAGGING_DATE_KEY;
+import static org.duraspace.bagit.BagConfig.BAG_SIZE_KEY;
+import static org.duraspace.bagit.BagConfig.CONTACT_EMAIL_KEY;
+import static org.duraspace.bagit.BagConfig.CONTACT_NAME_KEY;
+import static org.duraspace.bagit.BagConfig.CONTACT_PHONE_KEY;
+import static org.duraspace.bagit.BagConfig.EXTERNAL_DESCRIPTION_KEY;
+import static org.duraspace.bagit.BagConfig.ORGANIZATION_ADDRESS_KEY;
+import static org.duraspace.bagit.BagConfig.PAYLOAD_OXUM_KEY;
+import static org.duraspace.bagit.BagConfig.SOURCE_ORGANIZATION_KEY;
+import static org.duraspace.bagit.BagConfig.TITLE_KEY;
+import static org.duraspace.bagit.BagProfileConstants.ACCEPT_BAGIT_VERSION;
+import static org.duraspace.bagit.BagProfileConstants.ACCEPT_SERIALIZATION;
+import static org.duraspace.bagit.BagProfileConstants.BAGIT_PROFILE_IDENTIFIER;
+import static org.duraspace.bagit.BagProfileConstants.BAGIT_PROFILE_INFO;
+import static org.duraspace.bagit.BagProfileConstants.BAGIT_PROFILE_VERSION;
+import static org.duraspace.bagit.BagProfileConstants.BAG_INFO;
+import static org.duraspace.bagit.BagProfileConstants.MANIFESTS_REQUIRED;
+import static org.duraspace.bagit.BagProfileConstants.PROFILE_VERSION;
+import static org.duraspace.bagit.BagProfileConstants.TAG_FILES_REQUIRED;
+import static org.duraspace.bagit.BagProfileConstants.TAG_MANIFESTS_REQUIRED;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -40,10 +61,18 @@ public class BagProfileTest {
 
     private final String testValue = "test-value";
     private final String defaultBag = "/bag";
+
+    // profile locations
     private final String defaultProfilePath = "profiles/profile.json";
     private final String extraTagsPath = "profiles/profileWithExtraTags.json";
     private final String invalidPath = "profiles/invalidProfile.json";
     private final String invalidSerializationPath = "profiles/invalidProfileSerializationError.json";
+
+    // config locations
+    private final String bagitConfig = "configs/bagit-config.yml";
+    private final String bagitConfigBadAccess = "configs/bagit-config-bad-access.yml";
+    private final String bagitConfigMissingAccess = "configs/bagit-config-missing-access.yml";
+    private final String bagitConfigNoAptrust = "configs/bagit-config-no-aptrust.yml";
 
     private final Version defaultVersion = new Version(1, 0);
     private final String targetDir = "src/test/resources/sample";
@@ -68,16 +97,16 @@ public class BagProfileTest {
         assertTrue(profile.getTagDigestAlgorithms().contains(sha256.bagitName()));
         assertTrue(profile.getTagDigestAlgorithms().contains(sha512.bagitName()));
 
-        assertTrue(profile.getMetadataFields().get("Source-Organization").isRequired());
-        assertTrue(profile.getMetadataFields().get("Organization-Address").isRequired());
-        assertTrue(profile.getMetadataFields().get("Contact-Name").isRequired());
-        assertTrue(profile.getMetadataFields().get("Contact-Phone").isRequired());
-        assertTrue(profile.getMetadataFields().get("Bag-Size").isRequired());
-        assertTrue(profile.getMetadataFields().get("Bagging-Date").isRequired());
-        assertTrue(profile.getMetadataFields().get("Payload-Oxum").isRequired());
-        assertFalse(profile.getMetadataFields().get("Contact-Email").isRequired());
+        assertTrue(profile.getMetadataFields().get(SOURCE_ORGANIZATION_KEY).isRequired());
+        assertTrue(profile.getMetadataFields().get(ORGANIZATION_ADDRESS_KEY).isRequired());
+        assertTrue(profile.getMetadataFields().get(CONTACT_NAME_KEY).isRequired());
+        assertTrue(profile.getMetadataFields().get(CONTACT_PHONE_KEY).isRequired());
+        assertTrue(profile.getMetadataFields().get(BAG_SIZE_KEY).isRequired());
+        assertTrue(profile.getMetadataFields().get(BAGGING_DATE_KEY).isRequired());
+        assertTrue(profile.getMetadataFields().get(PAYLOAD_OXUM_KEY).isRequired());
+        assertFalse(profile.getMetadataFields().get(CONTACT_EMAIL_KEY).isRequired());
 
-        assertTrue(profile.getSectionNames().stream().allMatch(t -> t.equalsIgnoreCase(BagProfileConstants.BAG_INFO)));
+        assertTrue(profile.getSectionNames().stream().allMatch(t -> t.equalsIgnoreCase(BAG_INFO)));
 
         assertFalse(profile.isAllowFetch());
         assertEquals(BagProfile.Serialization.OPTIONAL, profile.getSerialization());
@@ -95,21 +124,17 @@ public class BagProfileTest {
         final String aptrustInfo = "APTrust-Info";
         final BagProfile profile = new BagProfile(Files.newInputStream(resolveResourcePath(extraTagsPath)));
 
-        assertTrue(profile.getSectionNames().stream().anyMatch(t -> t.equalsIgnoreCase(BagProfileConstants.BAG_INFO)));
+        assertTrue(profile.getSectionNames().stream().anyMatch(t -> t.equalsIgnoreCase(BAG_INFO)));
         assertTrue(profile.getSectionNames().stream().anyMatch(t -> t.equals(aptrustInfo)));
         assertTrue(profile.getSectionNames().stream().noneMatch(t -> t.equals("Wrong-Tags")));
-        assertTrue(profile.getMetadataFields(aptrustInfo).containsKey("Title"));
-        assertTrue(profile.getMetadataFields(aptrustInfo).containsKey("Access"));
-        assertTrue(profile.getMetadataFields(aptrustInfo).get("Access").getValues().contains("Consortia"));
-        assertTrue(profile.getMetadataFields(aptrustInfo).get("Access").getValues().contains("Institution"));
-        assertTrue(profile.getMetadataFields(aptrustInfo).get("Access").getValues().contains("Restricted"));
+        assertTrue(profile.getMetadataFields(aptrustInfo).containsKey(TITLE_KEY));
+        assertTrue(profile.getMetadataFields(aptrustInfo).containsKey(ACCESS_KEY));
+        assertTrue(profile.getMetadataFields(aptrustInfo).get(ACCESS_KEY).getValues().contains("Consortia"));
+        assertTrue(profile.getMetadataFields(aptrustInfo).get(ACCESS_KEY).getValues().contains("Institution"));
+        assertTrue(profile.getMetadataFields(aptrustInfo).get(ACCESS_KEY).getValues().contains("Restricted"));
 
     }
 
-    private final String bagitConfig = "configs/bagit-config.yml";
-    private final String bagitConfigBadAccess = "configs/bagit-config-bad-access.yml";
-    private final String bagitConfigMissingAccess = "configs/bagit-config-missing-access.yml";
-    private final String bagitConfigNoAptrust = "configs/bagit-config-no-aptrust.yml";
 
     @Test
     public void testGoodConfig() throws Exception {
@@ -175,13 +200,13 @@ public class BagProfileTest {
         } catch (RuntimeException e) {
             final String message = e.getMessage();
             // check that the error message contains each failed section
-            Assert.assertTrue(message.contains(BagProfileConstants.BAGIT_PROFILE_INFO));
-            Assert.assertTrue(message.contains(BagProfileConstants.BAGIT_PROFILE_IDENTIFIER));
-            Assert.assertTrue(message.contains(BagProfileConstants.ACCEPT_SERIALIZATION));
-            Assert.assertTrue(message.contains(BagProfileConstants.MANIFESTS_REQUIRED));
-            Assert.assertTrue(message.contains(BagProfileConstants.TAG_MANIFESTS_REQUIRED));
-            Assert.assertTrue(message.contains(BagProfileConstants.TAG_FILES_REQUIRED));
-            Assert.assertTrue(message.contains(BagProfileConstants.ACCEPT_BAGIT_VERSION));
+            Assert.assertTrue(message.contains(BAGIT_PROFILE_INFO));
+            Assert.assertTrue(message.contains(BAGIT_PROFILE_IDENTIFIER));
+            Assert.assertTrue(message.contains(ACCEPT_SERIALIZATION));
+            Assert.assertTrue(message.contains(MANIFESTS_REQUIRED));
+            Assert.assertTrue(message.contains(TAG_MANIFESTS_REQUIRED));
+            Assert.assertTrue(message.contains(TAG_FILES_REQUIRED));
+            Assert.assertTrue(message.contains(ACCEPT_BAGIT_VERSION));
         }
     }
 
@@ -233,11 +258,11 @@ public class BagProfileTest {
         final StringBuilder errors = new StringBuilder();
 
         // Bag-Profile-Info
-        final List<String> expectedInfoFields = Arrays.asList(BagConfig.SOURCE_ORGANIZATION_KEY,
-                                                              BagConfig.EXTERNAL_DESCRIPTION_KEY,
-                                                              BagProfileConstants.PROFILE_VERSION,
-                                                              BagProfileConstants.BAGIT_PROFILE_IDENTIFIER,
-                                                              BagProfileConstants.BAGIT_PROFILE_VERSION);
+        final List<String> expectedInfoFields = Arrays.asList(SOURCE_ORGANIZATION_KEY,
+                                                              EXTERNAL_DESCRIPTION_KEY,
+                                                              PROFILE_VERSION,
+                                                              BAGIT_PROFILE_IDENTIFIER,
+                                                              BAGIT_PROFILE_VERSION);
         final Map<String, String> bagInfo = profile.getProfileMetadata();
         for (final String expected : expectedInfoFields) {
             if (!bagInfo.containsKey(expected)) {
@@ -399,7 +424,7 @@ public class BagProfileTest {
      * @param profile the BagProfile defining the required info fields
      */
     private void putRequiredBagInfo(final Bag bag, final BagProfile profile) {
-        final Map<String, ProfileFieldRule> bagInfoMeta = profile.getMetadataFields(BagProfileConstants.BAG_INFO);
+        final Map<String, ProfileFieldRule> bagInfoMeta = profile.getMetadataFields(BAG_INFO);
         for (Map.Entry<String, ProfileFieldRule> entry : bagInfoMeta.entrySet()) {
             if (entry.getValue().isRequired())  {
                 bag.getMetadata().add(entry.getKey(), testValue);
