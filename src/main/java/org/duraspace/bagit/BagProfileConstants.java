60219252dab95c3f9b82260e120669bcce781b09
Michael Ritter <emericaskate15@gmail.com>
2020-08-27 12:54:10 -0600

    Remove unused constants

--- src/main/java/org/duraspace/bagit/BagProfileConstants.java
+++ src/main/java/org/duraspace/bagit/BagProfileConstants.java
@@ -31,7 +31,4 @@ public abstract class BagProfileConstants {
 
     // misc
     public static final String BAGIT_TAG_SUFFIX = ".txt";
-    public static final String BAGIT_MD5 = "md5";
-    public static final String BAGIT_SHA1 = "sha1";
-    public static final String BAGIT_SHA_256 = "sha256";
 }
